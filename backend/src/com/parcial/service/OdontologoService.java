482b73c0591dde0b3693879e1c109e899b9f3c2d
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-05-25 20:24:08 -0300

    Se completa OdontologoService

--- backend/src/com/parcial/service/OdontologoService.java
+++ backend/src/com/parcial/service/OdontologoService.java
@@ -1,4 +1,24 @@
 package com.parcial.service;
 
+import com.parcial.dao.IDao;
+import com.parcial.entity.Odontologo;
+
+import java.util.List;
+
 public class OdontologoService {
+    private IDao<Odontologo> odontologoIDao;
+
+    public OdontologoService(IDao<Odontologo> odontologoIDao) {
+        this.odontologoIDao = odontologoIDao;
+    }
+
+    public Odontologo guardarOdontologo(Odontologo odontologo) {
+        return odontologoIDao.guardar(odontologo);
+    }
+    
+
+    public List<Odontologo> listarOdontologos() {
+        return odontologoIDao.listarTodos();
+    }
+
 }
