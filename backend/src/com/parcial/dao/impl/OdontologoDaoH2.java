74c994c487a74b055dcd65ad927f6409fa78cb7c
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-05-25 19:46:49 -0300

    Se agrego el metodo Guardar

--- backend/src/com/parcial/dao/impl/OdontologoDaoH2.java
+++ backend/src/com/parcial/dao/impl/OdontologoDaoH2.java
@@ -1,14 +1,62 @@
 package com.parcial.dao.impl;
 
+import com.parcial.dao.H2Connection;
 import com.parcial.dao.IDao;
 import com.parcial.entity.Odontologo;
+import org.apache.log4j.Logger;
 
+import java.sql.*;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao<Odontologo> {
+    private static final Logger LOGGER = Logger.getLogger(OdontologoDaoH2.class);
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        return null;
+        Connection connection = null;
+        try {
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO ODONTOLOGOS (NUMERO_DE_MATRICULA, NOMBRE, APELLIDO) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, odontologo.getNroDeMatricula());
+            ps.setString(2, odontologo.getNombre());
+            ps.setString(3, odontologo.getApellido());
+            ps.execute();
+
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            while (rs.next()) {
+                odontologo.setId(rs.getInt(1));
+            }
+
+            connection.commit();
+            LOGGER.info("Se ha registrado el odontologo: " + odontologo);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    System.out.println("Ocurrio un problema");
+                    e.printStackTrace();
+                } catch (SQLException exception) {
+                    LOGGER.error(exception.getMessage());
+                    exception.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                LOGGER.error("Ocurrido un error al intentar cerrar la Base de Datos" + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+
+
+        return odontologo;
     }
 
     @Override
