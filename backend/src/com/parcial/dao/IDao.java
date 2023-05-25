b4387472792f8c08e30392d0b36cbdcfa89cf5bf
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-05-25 19:29:38 -0300

    Se reorganizo la estructura

---
+++ backend/src/com/parcial/dao/IDao.java
@@ -0,0 +1,11 @@
+package com.parcial.dao;
+
+import java.util.List;
+
+public interface IDao<T> {
+
+    T guardar(T t);
+
+    List<T> listarTodos();
+
+}
---
+++ backend/src/com/parcial/dao/impl/OdontologoDaoH2.java
@@ -0,0 +1,18 @@
+package com.parcial.dao.impl;
+
+import com.parcial.dao.IDao;
+import com.parcial.entity.Odontologo;
+
+import java.util.List;
+
+public class OdontologoDaoH2 implements IDao<Odontologo> {
+    @Override
+    public Odontologo guardar(Odontologo odontologo) {
+        return null;
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        return null;
+    }
+}
