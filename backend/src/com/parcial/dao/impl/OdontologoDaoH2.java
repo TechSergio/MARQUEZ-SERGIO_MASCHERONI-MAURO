8989f6af18018473183c58522c465ea3c8b3df50
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-05-25 21:05:01 -0300

    Arreglando objeto Odontolo en listar todos

--- backend/src/com/parcial/dao/impl/OdontologoDaoH2.java
+++ backend/src/com/parcial/dao/impl/OdontologoDaoH2.java
@@ -72,7 +72,7 @@ public class OdontologoDaoH2 implements IDao<Odontologo> {
             PreparedStatement ps = connection.prepareStatement("SELECT * FROM ODONTOLOGOS");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                Odontologo odontologo = new Odontologo(rs.getInt(1), rs.getString(2), rs.getString(3));
+                Odontologo odontologo = new Odontologo(rs.getInt(1), rs.getInt(2),rs.getString(3), rs.getString(4));
                 odontologos.add(odontologo);
             }
 
