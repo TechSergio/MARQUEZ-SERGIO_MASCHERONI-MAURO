fd65fe0c7167302b0e66214be76e5ee476b2af01
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-05-25 21:19:14 -0300

    retocando Service Test

--- backend/src/com/parcial/service/OdontologoServiceTest.java
+++ backend/src/com/parcial/service/OdontologoServiceTest.java
@@ -4,15 +4,16 @@ import com.parcial.dao.impl.OdontologoDaoH2;
 import com.parcial.entity.Odontologo;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 public class OdontologoServiceTest {
-    private static Connection connection = null;
     private OdontologoService odontologoService = new OdontologoService(new OdontologoDaoH2());
+
 
     @Test
     public void listarTodosLosOdontologos(){
